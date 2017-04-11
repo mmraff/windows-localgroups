8dd9432d46368340ae27e2fc40f062001a7e1caf
Matthew Rafferty <mmraff@gmail.com>
2017-04-11 17:19:13 -0600

    added to list() tests in test suite

--- test/test.js
+++ test/test.js
@@ -11,10 +11,14 @@ describe('windows-localgroups module', function() {
 })
 
 // TODO:
+// * tests for getComment, synchronous
+// * tests for getComment, asynchronous
+// * tests for getMembers, synchronous
+// * tests for getMembers, asynchronous
 
 var refList
 
-describe('list() call', function() {
+describe('list() synchronous call', function() {
 
   // Here the reference data is collected, as a side effect of testing the
   // basic form of the function:
@@ -48,6 +52,23 @@ describe('list() call', function() {
     // varied from one call to the next; but this has not been seen.
   })
 
+  it('should throw an exception when first argument is invalid', function() {
+    var firstArgs = [ 42, true, {}, [] ]
+    firstArgs.forEach(function(el) {
+      expect(function(){ mod.list(el) })
+        .to.throw(Error, /Invalid type for first argument/)
+    })
+  })
+
+  it('should throw an exception when non-empty servername arg is given' +
+     ' without passing a callback', function() {
+    expect(function(){ mod.list(os.hostname()) })
+      .to.throw(Error, /Must provide callback when giving a hostname/)
+  })
+})
+
+describe('list() asynchronous call', function() {
+
   it('callback results should be the same as for no-arg call when passed a' +
      ' callback, with or without empty arg preceding', function(done) {
 
@@ -71,22 +92,27 @@ describe('list() call', function() {
     })
   })
 
-  it('should throw an exception when first argument is invalid', function() {
-    var firstArgs = [ 42, true, {}, [] ]
-    firstArgs.forEach(function(el) {
-      expect(function(){ mod.list(el) })
-        .to.throw(Error, /Invalid type for first argument/)
+  it('should return an exception when unknown hostname passed', function(done) {
+
+    var badName = "IWILLNOTCALLMYCOMPUTERANINAPPROPRIATENAME"
+    // Error passed back, and you're to blame - you give list() a badName
+    mod.list(badName, function(err, data) {
+      expect(err).to.be.an('error')
+      expect(err.message)
+        .to.match(/(access is denied)|(hostname is unrecognized)|(no RPC server available)/)
+      // NOTES:
+      // * I have not yet seen access denial, but I expect there can be situations...
+      // * No RPC Server case can be when the host is disconnected from the network,
+      //   and not running such a server itself
+      done()
     })
   })
+  // It doesn't make sense to include any other tests with a hostname (other than
+  //   the current host) because
+  // a) This script doesn't know what other hosts are defined on an arbitrary
+  //   local network
+  // b) This script can't know in advance whether the query to another host will
+  //   fail, even if its name is known.
 
-  it('should throw an exception when non-empty servername arg is given' +
-     ' without passing a callback', function() {
-    expect(function(){ mod.list(os.hostname()) })
-      .to.throw(Error, /Must provide callback when giving a hostname/)
-  })
 })
-
-// * Given badname = "IWILLNOTCALLMYCOMPUTERANINAPPROPRIATENAME", test that the
-//  following returns one of 2 exceptions thru the 1st arg of the callback...
-//  mod.list(badname, callback)
 
