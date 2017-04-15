0f82f41c7363bddb32f0ec4af146a37830cb2a8e
Matthew Rafferty <mmraff@gmail.com>
2017-04-15 15:21:20 -0600

    Finished package.json, README, test suite

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # windows-localgroups
 A native addon for node.js to query local groups on Windows platforms
+
+## Install
+<pre>
+C:\Users\myUser><b>npm install windows-localgroups</b>
+</pre>
+
+## Usage
+```js
+var localgroups = require('windows-localgroups')
+```
+For each of the three available functions, the user may expect the result of
+querying the localhost as the synchronous return value *if no callback function
+is added to the argument list*. Simply supply a callback function as the last
+argument to turn the call asynchronous.  
+A callback function is *required* if a host other than the localhost is to be
+queried.  
+
+**Note:** In some environments, the synchronous functions may throw an `Error` of
+`"access is denied to current user"`, and any of the asynchronous functions may
+pass this error to the callback. This would be a consequence of the user's
+permissions versus the particular security configuration of the domain.
+
+---
+## API
+
+### localgroups.list()
+*Synchronous.* Returns a list of all local groups defined on the local system.
+- Return: {Array} array of objects, each with two properties:  
+  * **name** {String} Name of local group
+  * **comment** {String | `null`} Descriptive comment
+
+### localgroups.list([hostname,] callback)
+*Asynchronous.* Passes a list of all local groups defined on the local system
+(or, optionally, on the system named by `hostname`) to the `callback` function.
+If `hostname` is given and it is unknown or cannot be accessed, an `Error` is
+passed back.
+- `hostname` {String} *Optional.*  
+  The name of a host in the domain to query. An empty value (`undefined`, `null`,
+  or empty string) may be passed to get the same effect as omitting this argument.
+- `callback` {Function}  
+  * **error** {Error | `null`}
+  * **data** {Array} as described for return value of synchronous version,
+  if no error.
+
+### localgroups.getMembers(groupName)
+*Synchronous.* Returns a list of member data for the named group if it is known
+on the local system; otherwise an `Error` is thrown.
+- `groupName` {String} Name of local group.
+- Return: {Array} array of objects, each with three properties:  
+  * **sid** {String} The unique security identifier (SID) of the member **TODO: MSDN link**
+  * **accountType** {String} one of the following:  
+    `"user"`, `"group"`, `"well-known group"`, `"deleted"`, `"unknown"`
+  * **domainAndName** {String} The domain-qualified name of the member  
+    (i.e. `"DomainName\\AccountName"`)
+
+### localgroups.getMembers(groupName [, hostname], callback)
+*Asynchronous.* Passes a list of all member data for the named group (optionally
+on the system named by `hostname`) to the `callback` function, if the local group
+name is known; otherwise an `Error` is passed to the callback. If `hostname` is
+given and it is unknown or cannot be accessed, an `Error` is passed back.
+- `groupName` {String} Name of local group.
+- `hostname` {String} *Optional.*  
+  The name of a host in the domain to query. An empty value (`undefined`, `null`,
+  or empty string) may be passed to get the same effect as omitting this argument.
+- `callback` {Function}  
+  * **error** {Error | `null`}
+  * **data** {Array} as described for return value of synchronous version,
+    if no error.
+
+### localgroups.getComment(groupName)
+*Synchronous.* Returns the descriptive comment (or `null` if none) for the named
+group if it is known on the local system; otherwise an `Error` is thrown.
+- `groupName` {String} Name of local group.
+- Return: {String | `null`} The comment associated with the named group
+
+### localgroups.getComment(groupName [, hostname], callback)
+*Asynchronous.* Passes the descriptive comment (or `null` if none) for the named
+group (optionally on the system named by `hostname`) to the `callback` function,
+if the local group name is known; otherwise an `Error` is passed to the callback.
+If `hostname` is given and it is unknown or cannot be accessed, an `Error` is
+passed back.
+- `groupName` {String} Name of local group.
+- `hostname` {String} *Optional.*  
+  The name of a host in the domain to query. An empty value (`undefined`, `null`,
+  or empty string) may be passed to get the same effect as omitting this argument.
+- `callback` {Function}  
+  * **error** {Error | `null`}
+  * **data** {String | `null`} The comment associated with the named group,
+    if no error.
+
+------
+
+**License: MIT**
+
--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
-var os = require('os')
+var assert = require('assert')
+  , os = require('os')
   , expect = require('chai').expect
   , mod = require('../')
+  // This will be the results returned by mod.list(), used throughout the suite:
+  , refList
+
+if (process.platform !== 'win32') {
+  console.error('This module is only meant for Windows platforms.\n' +
+    'Aborting tests.\n');
+  return
+}
 
 describe('windows-localgroups module', function() {
   it('should export functions: list, getComment, getMembers', function() {
@@ -8,76 +17,100 @@ describe('windows-localgroups module', function() {
     expect(mod.getComment).to.be.a('function')
     expect(mod.getMembers).to.be.a('function')
   })
-})
 
-// TODO:
-// * tests for getComment, synchronous
-// * tests for getComment, asynchronous
-// * tests for getMembers, synchronous
-// * tests for getMembers, asynchronous
+var emptyArgs   = [ undefined, null, '', new String() ]
+  , invalidArgs = [ 42, true, {}, [] ]
+  , badName = "IWILLNOTCALLMYCOMPUTERANINAPPROPRIATENAME"
+  , badGrpName = "Nobody Would Give A Group A Stupid Name Like This"
 
-var refList
+  , re_errNoGrpName = /Must provide name of local group/
+  , re_errEmptyGrpName = /Group name cannot be empty/
+  , re_errGrpNameType = /Invalid type for group name argument/
+  , re_errArg1Type = /Invalid type for first argument/
+  , re_errArg2Type = /Invalid type for second argument/
+  , re_errNoCb = /Must provide callback when giving a hostname/
+  , re_errNotFound = /(could not be found)|(does not exist)/
+  , re_errHostnameAccess =
+     /(access is denied)|(hostname is unrecognized)|(no RPC server available)/
+
+function dummyFunc(err, data) {
+  assert(false, 'This dummy function should never get called!')
+}
 
 describe('list() synchronous call', function() {
 
   // Here the reference data is collected, as a side effect of testing the
   // basic form of the function:
-  it('should return an array when no arguments are given', function() {
+  before('should return an array when no arguments are given', function() {
     refList = mod.list()
     expect(refList).to.be.instanceof(Array)
+    if (refList.length == 0) {
+      console.warn(
+        'NO LOCAL GROUPS DEFINED ON THIS SYSTEM!\n' +
+        'NO MEANINGFUL TESTS CAN BE DONE, SO TEST SUITE WILL BE ABORTED.\n' +
+        'SORRY!'
+      );
+      process.exit()
+    }
   })
 
   it('results should contain only objects, ' +
      'each with string-value properties "name" and "comment"', function() {
 
-    var list = refList
-    for (var i = 0; i < list.length; i++) {
-      expect(list[i]).to.be.an('object')
-      expect(list[i]).to.have.property('name').that.is.a('string').that.is.not.empty
-      expect(list[i]).to.have.property('comment')
-      if (list[i].comment !== null)
-        expect(list[i].comment).to.be.a('string')
+    for (var i = 0; i < refList.length; i++) {
+      var item = refList[i]
+      expect(item).to.be.an('object')
+      expect(item).to.have.property('name').that.is.a('string').that.is.not.empty
+      expect(item).to.have.property('comment')
+      if (item.comment !== null)
+        expect(item.comment).to.be.a('string')
     }
   })
 
   it('results should be the same as for no-arg call when passed a single' +
      ' empty arg (undefined, null, empty string)', function() {
 
-    var firstArgs = [ undefined, null, '', new String() ]
-
-    firstArgs.forEach(function(el) {
+    emptyArgs.forEach(function(el) {
       expect(mod.list(el)).to.deep.equal(refList)
     })
     // This would fail if the order of elements returned by NetLocalGroupEnum
     // varied from one call to the next; but this has not been seen.
   })
 
-  it('should throw an exception when first argument is invalid', function() {
-    var firstArgs = [ 42, true, {}, [] ]
-    firstArgs.forEach(function(el) {
-      expect(function(){ mod.list(el) })
-        .to.throw(Error, /Invalid type for first argument/)
-    })
+  it('should throw an exception when 1st argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.list(invalidArgs[i]) })
+        .to.throw(Error, re_errArg1Type)
+    }
   })
 
   it('should throw an exception when non-empty servername arg is given' +
      ' without passing a callback', function() {
-    expect(function(){ mod.list(os.hostname()) })
-      .to.throw(Error, /Must provide callback when giving a hostname/)
+
+    // Name doesn't matter - if given without callback, it is rejected
+    expect(function(){ mod.list('BATCAVE') })
+      .to.throw(Error, re_errNoCb)
   })
 })
 
 describe('list() asynchronous call', function() {
 
+  it('should pass back an exception when 1st argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.list(invalidArgs[i], dummyFunc) })
+        .to.throw(Error, re_errArg1Type)
+    }
+  })
+
   it('callback results should be the same as for no-arg call when passed a' +
      ' callback, with or without empty arg preceding', function(done) {
 
-    var firstArgs = [ undefined, null, '', new String() ]
-
     function nextTest(i) {
-      if (firstArgs.length <= i) return done()
+      if (emptyArgs.length <= i) return done()
 
-      mod.list(firstArgs[i], function(err, data) {
+      mod.list(emptyArgs[i], function(err, data) {
         if (err) return done(err)
         expect(data).to.deep.equal(refList)
 
@@ -92,17 +125,16 @@ describe('list() asynchronous call', function() {
     })
   })
 
-  it('should return an exception when unknown hostname passed', function(done) {
+  it('should pass back an error when unknown hostname passed', function(done) {
 
-    var badName = "IWILLNOTCALLMYCOMPUTERANINAPPROPRIATENAME"
     // Error passed back, and you're to blame - you give list() a badName
     mod.list(badName, function(err, data) {
       expect(err).to.be.an('error')
       expect(err.message)
-        .to.match(/(access is denied)|(hostname is unrecognized)|(no RPC server available)/)
+        .to.match(re_errHostnameAccess)
       // NOTES:
       // * I have not yet seen access denial, but I expect there can be situations...
-      // * No RPC Server case can be when the host is disconnected from the network,
+      // * 'No RPC Server' case can be when the host is disconnected from the network,
       //   and not running such a server itself
       done()
     })
@@ -113,6 +145,394 @@ describe('list() asynchronous call', function() {
   //   local network
   // b) This script can't know in advance whether the query to another host will
   //   fail, even if its name is known.
+
+})
+
+describe('getComment() synchronous call', function() {
+
+  it('should throw an exception when no argument given', function() {
+    expect(function(){ mod.getComment() }).to.throw(Error, re_errNoGrpName)
+  })
+
+  it('should throw an exception when passed a single empty value', function() {
+
+    expect(function(){ mod.getComment(undefined) })
+      .to.throw(Error, re_errNoGrpName)
+    expect(function(){ mod.getComment(null) })
+      .to.throw(Error, re_errNoGrpName)
+
+    expect(function(){ mod.getComment('') })
+      .to.throw(Error, re_errEmptyGrpName)
+    expect(function(){ mod.getComment(new String()) })
+      .to.throw(Error, re_errEmptyGrpName)
+  })
+
+  it('should throw an exception when 1st argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getComment(invalidArgs[i]) })
+        .to.throw(Error, re_errGrpNameType)
+    }
+  })
+
+  it('should throw an exception when 2nd argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getComment(refList[0].name, invalidArgs[i]) })
+        .to.throw(Error, re_errArg2Type)
+    }
+  })
+
+  it('should throw an exception when non-empty hostname arg is given' +
+     ' without passing a callback', function() {
+
+    // Here we'll use a group name we know is valid; but that, and the hostname
+    // used, doesn't matter - if hostname given without callback, it is rejected
+    expect(function(){ mod.getComment(refList[0].name, 'BATCAVE') })
+      .to.throw(Error, re_errNoCb)
+  })
+
+  it('should throw an exception when given an unknown group name', function() {
+
+    expect(function(){ mod.getComment(badGrpName) })
+      .to.throw(Error, re_errNotFound)
+  })
+
+  it('should return a matching comment for each group enumerated by list()', function() {
+
+    for (var i = 0; i < refList.length; i++) {
+      expect(mod.getComment(refList[i].name)).to.equal(refList[i].comment)
+    }
+
+    for (var i = 0; i < refList.length; i++) {
+      emptyArgs.forEach(function(el) {
+        expect(mod.getComment(refList[i].name, el)).to.equal(refList[i].comment)
+      })
+    }
+  })
+
+})
+
+describe('getComment() asynchronous call', function() {
+
+  it('should throw an exception when no group name given', function() {
+    // NOTE: because there is a 1st argument (the callback) given,
+    // the error is not the same as when there are no arguments given.
+    expect(function(){ mod.getComment(dummyFunc) })
+      .to.throw(Error, re_errGrpNameType)
+  })
+
+  it('should throw an exception when passed an empty arg for group name', function() {
+
+    expect(function(){ mod.getComment(undefined, dummyFunc) })
+      .to.throw(Error, re_errNoGrpName)
+    expect(function(){ mod.getComment(null, dummyFunc) })
+      .to.throw(Error, re_errNoGrpName)
+
+    expect(function(){ mod.getComment('', dummyFunc) })
+      .to.throw(Error, re_errEmptyGrpName)
+    expect(function(){ mod.getComment(new String(), dummyFunc) })
+      .to.throw(Error, re_errEmptyGrpName)
+  })
+
+  it('should throw an exception when 1st argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getComment(invalidArgs[i], dummyFunc) })
+        .to.throw(Error, re_errGrpNameType)
+    }
+  })
+
+  it('should throw an exception when 2nd argument has invalid type', function() {
+
+    var grpName0 = refList[0].name
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getComment(grpName0, invalidArgs[i], dummyFunc) })
+        .to.throw(Error, re_errArg2Type)
+    }
+  })
+
+  it('should pass back an error when given an unknown group name', function(done) {
+
+    mod.getComment(badGrpName, function(err1, data) {
+      expect(err1).to.be.an('error')
+      expect(err1.message).to.match(re_errNotFound)
+
+      mod.getComment(badGrpName, '', function(err2, data) {
+        expect(err2).to.be.an('error')
+        expect(err2.message).to.match(re_errNotFound)
+
+        mod.getComment(badGrpName, os.hostname(), function(err3, data) {
+          expect(err3).to.be.an('error')
+          expect(err3.message).to.match(re_errNotFound)
+          done()
+        })
+      })
+    })
+  })
+
+  it('should pass back an error when unknown hostname passed', function(done) {
+
+    mod.getComment(refList[0].name, badName, function(err, data) {
+      expect(err).to.be.an('error')
+      expect(err.message).to.match(re_errHostnameAccess)
+      done()
+    })
+  })
+
+  it('should pass back a matching comment for each group enumerated by list()'
+     + ' (given no hostname)', function(done) {
+
+    function nextTest(i) {
+      if (i >= refList.length) return done()
+
+      mod.getComment(refList[i].name, function(err, data) {
+        if (err) return done(err)
+        expect(data).to.equal(refList[i].comment)
+        return nextTest(i + 1)
+      })
+    }
+
+    nextTest(0)
+  })
+
+  it('should pass back a matching comment for each group enumerated by list()'
+     + ' (given null for hostname)', function(done) {
+
+    function nextTestEmptyHostname(i) {
+      if (i >= refList.length) return done()
+
+      mod.getComment(refList[i].name, null, function(err, data) {
+        if (err) return done(err)
+        expect(data).to.equal(refList[i].comment)
+        return nextTestEmptyHostname(i + 1)
+      })
+    }
+
+    nextTestEmptyHostname(0)
+  })
+
+  it('should pass back a matching comment for each group enumerated by list()'
+     + ' (given local hostname)', function(done) {
+
+    function nextTestRealHostname(i) {
+      if (i >= refList.length) return done()
+
+      mod.getComment(refList[i].name, os.hostname(), function(err, data) {
+        if (err) return done(err)
+        expect(data).to.equal(refList[i].comment)
+        return nextTestRealHostname(i + 1)
+      })
+    }
+
+    nextTestRealHostname(0)
+  })
+
+})
+
+describe('getMembers() synchronous call', function() {
+
+  before('TODO?', function() {
+  })
+
+  it('should throw an exception when no argument given', function() {
+    expect(function(){ mod.getMembers() }).to.throw(Error, re_errNoGrpName)
+  })
+
+  it('should throw an exception when passed a single empty value', function() {
+
+    expect(function(){ mod.getMembers(undefined) })
+      .to.throw(Error, re_errNoGrpName)
+    expect(function(){ mod.getMembers(null) })
+      .to.throw(Error, re_errNoGrpName)
+
+    expect(function(){ mod.getMembers('') })
+      .to.throw(Error, re_errEmptyGrpName)
+    expect(function(){ mod.getMembers(new String()) })
+      .to.throw(Error, re_errEmptyGrpName)
+  })
+
+  it('should throw an exception when 1st argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getMembers(invalidArgs[i]) })
+        .to.throw(Error, re_errGrpNameType)
+    }
+  })
+
+  it('should throw an exception when 2nd argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getMembers(refList[0].name, invalidArgs[i]) })
+        .to.throw(Error, re_errArg2Type)
+    }
+  })
+
+  it('should throw an exception when non-empty hostname arg is given' +
+     ' without passing a callback', function() {
+
+    expect(function(){ mod.getMembers(refList[0].name, 'BATCAVE') })
+      .to.throw(Error, re_errNoCb)
+  })
+
+  it('should throw an exception when given an unknown group name', function() {
+
+    expect(function(){ mod.getMembers(badGrpName) })
+      .to.throw(Error, re_errNotFound)
+  })
+
+  it('should return an array for each group enumerated by list()', function() {
+
+    // Here we collect the member lists as a side effect of the test, so that
+    // we retain the data for the next test
+    for (var i = 0; i < refList.length; i++) {
+      refList[i].members = mod.getMembers(refList[i].name)
+      expect(refList[i].members).to.be.instanceof(Array)
+    }
+  })
+
+  it('results should contain only objects, each with string-value properties' +
+     ' "sid", "accountType" and "domainAndName"', function() {
+
+    for (var i = 0; i < refList.length; i++) {
+      var item = refList[i]
+      for (var n = 0; n < item.members.length; n++) {
+        var member = item.members[n]
+        expect(member).to.be.an('object')
+        expect(member).to.have.property('sid').that.is.a('string').that.is.not.empty
+        expect(member).to.have.property('accountType').that.is.a('string').that.is.not.empty
+        expect(member).to.have.property('domainAndName').that.is.a('string').that.is.not.empty
+      }
+    }
+  })
+})
+
+describe('getMembers() asynchronous call', function() {
+
+  it('should throw an exception when no group name given', function() {
+    // NOTE: because there is a 1st argument (the callback) given,
+    // the error is not the same as when there are no arguments given.
+    expect(function(){ mod.getMembers(dummyFunc) })
+      .to.throw(Error, re_errGrpNameType)
+  })
+
+  it('should throw an exception when passed an empty arg for group name', function() {
+
+    expect(function(){ mod.getMembers(undefined, dummyFunc) })
+      .to.throw(Error, re_errNoGrpName)
+    expect(function(){ mod.getMembers(null, dummyFunc) })
+      .to.throw(Error, re_errNoGrpName)
+
+    expect(function(){ mod.getMembers('', dummyFunc) })
+      .to.throw(Error, re_errEmptyGrpName)
+    expect(function(){ mod.getMembers(new String(), dummyFunc) })
+      .to.throw(Error, re_errEmptyGrpName)
+  })
+
+  it('should throw an exception when 1st argument has invalid type', function() {
+
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getMembers(invalidArgs[i], dummyFunc) })
+        .to.throw(Error, re_errGrpNameType)
+    }
+  })
+
+  it('should throw an exception when 2nd argument has invalid type', function() {
+
+    var grpName0 = refList[0].name
+    for (var i = 0; i < invalidArgs.length; i++) {
+      expect(function(){ mod.getMembers(grpName0, invalidArgs[i], dummyFunc) })
+        .to.throw(Error, re_errArg2Type)
+    }
+  })
+
+  it('should pass back an error when given an unknown group name', function(done) {
+
+    mod.getMembers(badGrpName, function(err1, data) {
+      expect(err1).to.be.an('error')
+      expect(err1.message).to.match(re_errNotFound)
+
+      mod.getMembers(badGrpName, '', function(err2, data) {
+        expect(err2).to.be.an('error')
+        expect(err2.message).to.match(re_errNotFound)
+
+        mod.getMembers(badGrpName, os.hostname(), function(err3, data) {
+          expect(err3).to.be.an('error')
+          expect(err3.message).to.match(re_errNotFound)
+          done()
+        })
+      })
+    })
+  })
+
+  it('should pass back an error when unknown hostname passed', function(done) {
+
+    mod.getMembers(refList[0].name, badName, function(err, data) {
+      expect(err).to.be.an('error')
+      expect(err.message).to.match(re_errHostnameAccess)
+      done()
+    })
+  })
+
+  function validateMemberData(list) {
+    expect(list).to.be.instanceof(Array)
+
+    for (var n = 0; n < list.length; n++) {
+      expect(list[n]).to.be.an('object')
+      expect(list[n]).to.have.property('sid').that.is.a('string').that.is.not.empty
+      expect(list[n]).to.have.property('accountType').that.is.a('string').that.is.not.empty
+      expect(list[n]).to.have.property('domainAndName').that.is.a('string').that.is.not.empty
+    }
+  }
+
+  it('should pass back valid member data for each group enumerated by list()'
+     + ' (given no hostname)', function(done) {
+
+    function nextTest(i) {
+      if (i >= refList.length) return done()
+
+      mod.getMembers(refList[i].name, function(err, data) {
+        if (err) return done(err)
+        validateMemberData(data)
+        return nextTest(i + 1)
+      })
+    }
+
+    nextTest(0)
+  })
+
+  it('should pass back valid member data for each group enumerated by list()'
+     + ' (given null for hostname)', function(done) {
+
+    function nextTestEmptyHostname(i) {
+      if (i >= refList.length) return done()
+
+      mod.getMembers(refList[i].name, null, function(err, data) {
+        if (err) return done(err)
+        validateMemberData(data)
+        return nextTestEmptyHostname(i + 1)
+      })
+    }
+
+    nextTestEmptyHostname(0)
+  })
+
+  it('should pass back valid member data for each group enumerated by list()'
+     + ' (given local hostname)', function(done) {
+
+    function nextTestRealHostname(i) {
+      if (i >= refList.length) return done()
+
+      mod.getMembers(refList[i].name, os.hostname(), function(err, data) {
+        if (err) return done(err)
+        validateMemberData(data)
+        return nextTestRealHostname(i + 1)
+      })
+    }
+
+    nextTestRealHostname(0)
+  })
+})
 
 })
 
